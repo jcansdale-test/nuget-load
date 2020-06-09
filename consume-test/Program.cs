4b099674e56ed567ac749b10f00f7d3bf4b912c2
Jamie Cansdale <jcansdale@gmail.com>
2020-06-09 15:34:39 +0000

    Move setup script

--- consume-test/Program.cs
+++ consume-test/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+using System;
+using System.Reflection;
 
 namespace consume_test
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var assemblyName = "nuget-load";
+            var asm = Assembly.Load(assemblyName);
+            var version = asm.GetName().Version.ToString();
+            Console.WriteLine($"Assembly version: {version}");
+
+            if(args.Length > 0)
+            {
+                var expectedVersion = args[0];
+                if (version == expectedVersion)
+                {
+                    Console.WriteLine($"Assembly `{assemblyName}` was expected version `{expectedVersion}`!");
+                    return 0;
+                }
+                else
+                {
+                    Console.WriteLine($"Assembly `{assemblyName}` was `{version}` not expected version `{expectedVersion}`!");
+                    return 1;
+                }
+            }
+
+            return 0;
         }
     }
 }
